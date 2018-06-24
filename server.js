cdcf157c4e314f00df309ec791d29ab8fd8e37bc
Lê Bá Công <lebacong1409it@gmail.com>
2018-06-24 22:48:15 +0700

    .

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
+app.listen(process.env.PORT || 8080);
 
+
+//Path Location Strategy
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200, () => {
-    console.log('Connected to Port'); //Listening on port 4201
-});
+console.log('Console Listening');
